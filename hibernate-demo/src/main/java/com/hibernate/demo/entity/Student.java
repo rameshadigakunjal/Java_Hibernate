30246ce72955bc8ee33457b23524c962cb6ea317
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2025-12-25 10:52:15 +0530

    Add Hibernate project structure

---
+++ hibernate-demo/src/main/java/com/hibernate/demo/entity/Student.java
@@ -0,0 +1,42 @@
+package com.hibernate.demo.entity;
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "student")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private int age;
+
+    public Student() {}
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ hibernate-demo/src/main/java/com/hibernate/demo/main/SaveStudent.java
@@ -0,0 +1,27 @@
+package com.hibernate.demo.main;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.hibernate.demo.entity.Student;
+import com.hibernate.demo.util.HibernateUtil;
+
+public class SaveStudent {
+
+    public static void main(String[] args) {
+
+        Session session = HibernateUtil
+                .getSessionFactory()
+                .openSession();
+
+        Transaction tx = session.beginTransaction();
+
+        Student student = new Student("Ramesh", 23);
+        session.save(student);
+
+        tx.commit();
+        session.close();
+
+        System.out.println("Student saved successfully!");
+    }
+}
